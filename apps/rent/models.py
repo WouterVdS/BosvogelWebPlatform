fa2083dce5fd65e4ba3941c918b7ef7197e2fc56
Wouter Van der Schraelen <wvanderschraelen@gmail.com>
2019-03-12 23:29:41 +0100

    Added some tests to RentReservation model

--- apps/rent/models.py
+++ apps/rent/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.dispatch import receiver
 
 from apps.agenda.models import Event
 from apps.place.models import Place
+from apps.rent.validators import validate_international_phone_number, validate_iban_format
 
 NEW_REQUEST = 'NR'
 COMMUNICATING = 'CO'
 AWAITING_CONTRACT = 'AC'
 AWAITING_DEPOSIT = 'AD'
 APPROVED = 'AP'
-HAPPENING_NOW = 'HN'  # todo check on period and current date + auto set this field
 AWAITING_PAYMENT = 'AP'
 CANCELLED = 'CA'
 DONE = 'DO'
@@ -19,7 +20,6 @@ RESERVATION_STATUSES = (
     (AWAITING_CONTRACT, 'Wachten op contract'),
     (AWAITING_DEPOSIT, 'Wachten op storting voorschot'),
     (APPROVED, 'Goedgekeurd'),
-    (HAPPENING_NOW, 'Verhuurperiode nu bezig'),
     (AWAITING_PAYMENT, 'Afwachten betaling eindafrekening'),
     (CANCELLED, 'Afgezegd'),
     (DONE, 'Afgelopen'),
@@ -38,10 +38,11 @@ DEPOSIT_STATUSES = (
 
 class RentReservation(models.Model):
     groupName = models.CharField(max_length=64)
+    town = models.CharField(max_length=32)
     email = models.EmailField()
-    phoneNr = models.CharField(max_length=13)  # todo checks
-    period = models.ForeignKey(to=Event, on_delete=models.CASCADE)
-    bankAccountNumber = models.CharField(max_length=16)  # todo more checks (use IBAN checksum)
+    phoneNr = models.CharField(max_length=13, validators=[validate_international_phone_number])
+    period = models.ForeignKey(null=True, to=Event, on_delete=models.SET_NULL)
+    bankAccountNumber = models.CharField(max_length=19, validators=[validate_iban_format])
     contract = models.FileField(null=True, blank=True)  # todo pick destination
     status = models.CharField(max_length=3, choices=RESERVATION_STATUSES, default=NEW_REQUEST)
     depositStatus = models.CharField(max_length=1, choices=DEPOSIT_STATUSES, default=AWAITING)
@@ -50,9 +51,15 @@ class RentReservation(models.Model):
     checklist = models.TextField(null=True, blank=True)  # todo convert to json?
     finalBill = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=2)
 
-    def __str__(self):
-        return self.groupName + ' ' + str(self.period)
-
     class Meta:
         verbose_name = 'Reservatie'
         verbose_name_plural = 'Reservaties'
+
+    def __str__(self):
+        return self.groupName + ' (' + self.town + '), ' + str(self.period.startDate) + ' - ' + str(self.period.endDate)
+
+
+@receiver(models.signals.post_delete, sender=RentReservation)
+def handle_deleted_profile(sender, instance, **kwargs):
+    if instance.period:
+        instance.period.delete()
--- apps/rent/tests/test_models.py
+++ apps/rent/tests/test_models.py
@@ -1,1 +1,83 @@
-# todo tests
+from datetime import date
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from apps.agenda.models import Event
+from apps.home.constants import Events
+from apps.rent.models import RentReservation
+
+
+class RentReservationTestCase(TestCase):
+    a_valid_event = None
+    minimal_valid_reservation = None
+
+    def setUp(self):
+        self.a_valid_event = Event.objects.create(startDate=date(2019, 7, 1),
+                                                  endDate=date(2019, 7, 11),
+                                                  type=Events.RENTAL)
+
+        self.minimal_valid_reservation = RentReservation.objects.create(groupName='De Bosvogels',
+                                                                        town='Grobbendonk',
+                                                                        email='info@bosvogels.be',
+                                                                        phoneNr='0032412345678',
+                                                                        period=self.a_valid_event,
+                                                                        bankAccountNumber='BE12 1234 5678 9876',
+                                                                        numberOfPeople=20)
+
+    def test_str_method(self):
+        # Check
+        self.assertEqual(str(self.minimal_valid_reservation),
+                         'De Bosvogels (Grobbendonk), 2019-07-01 - 2019-07-11',
+                         'String method should display something meaningful')
+
+    def test_required_fields(self):
+        # Check
+        self.minimal_valid_reservation.full_clean()
+
+    def test_validation_of_international_phoneNr(self):
+        # Build
+        reservation = self.minimal_valid_reservation
+
+        reservation.phoneNr = '0477556655'
+
+        # Operate
+        with self.assertRaises(ValidationError) as context:
+            reservation.full_clean()
+
+        # Check
+        self.assertTrue('should start with 0032' in str(context.exception),
+                        'But it is: ' + str(context.exception))
+
+    def test_validation_of_bankAccountNumber(self):
+        # Build
+        reservation = self.minimal_valid_reservation
+        reservation.bankAccountNumber = 'BE2144547wrong'
+
+        # Operate
+        with self.assertRaises(ValidationError) as context:
+            reservation.full_clean()
+
+        # Check
+        self.assertTrue('should be a valid bank' in str(context.exception),
+                        'But it is: ' + str(context.exception))
+
+    def test_event_deleted_when_reservation_deleted(self):
+        # Operate
+        RentReservation.objects.first().delete()
+
+        # Check
+        self.assertEqual(Event.objects.all().count(),
+                         0,
+                         'When a reservation is deleted, the corresponding event should be deleted')
+
+    def test_reservation_should_persist_if_event_gets_deleted(self):
+        # Operate
+        Event.objects.first().delete()
+
+        # Check
+        self.assertEqual(RentReservation.objects.all().count(),
+                         1,
+                         'When an event is somehow deleted, the corresponding reservation should persist')
+        self.assertIsNone(RentReservation.objects.first().period,
+                          'The period should be set to null in the database')
