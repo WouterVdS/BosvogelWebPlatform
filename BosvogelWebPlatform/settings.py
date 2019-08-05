1150d7d426e4c72082bb4aecefb507f27e132fc5
Wouter Van der Schraelen <wvanderschraelen@gmail.com>
2019-08-05 23:01:54 +0200

    Added procfile, gunicorn. Removed yaml

--- BosvogelWebPlatform/settings.py
+++ BosvogelWebPlatform/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 import environ
-import yaml
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
