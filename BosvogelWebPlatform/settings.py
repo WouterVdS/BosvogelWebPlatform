ce5a2976ac7a291b5e4aedaa7dbf5658216ff3be
Wouter Van der Schraelen <wvanderschraelen@gmail.com>
2019-08-06 07:55:15 +0200

    Django_heroku now only runs in Heroku environment

--- BosvogelWebPlatform/settings.py
+++ BosvogelWebPlatform/settings.py
@@ -29,6 +29,9 @@ DEFAULT_DEBUG = False
 
 env = environ.Env()
 DEBUG = env('DEBUG', default=DEFAULT_DEBUG)
+
+ENVIRONMENT = env('ENVIRONMENT', default='DEFAULT')
+
 SECRET_KEY = env('SECRET_KEY', default='*1ev7j$pn*he&0tn8o^12)tbi!e(h4w4^cxu8v(5*48z1syo-!')
 if not DEBUG and SECRET_KEY == '*1ev7j$pn*he&0tn8o^12)tbi!e(h4w4^cxu8v(5*48z1syo-!':  # pragma: no cover
     raise ImproperlyConfigured('Add the SECRET_KEY environment variable to overwrite the default one in production!')
@@ -137,4 +140,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Heroku auto configuration
-django_heroku.settings(locals())
+if ENVIRONMENT == 'HEROKU':
+    django_heroku.settings(locals())
